c9ea379fd864a917a3cfc4d70e4eb5fe94aaef45
hsc396612325 <heshucheng@xiyou3g.com>
2017-09-24 17:29:36 +0800

    和

--- app/src/main/java/fragemt/AboutFragment.java
+++ app/src/main/java/fragemt/AboutFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.xiyou3g.thefriendsofthecity.R;
 
@@ -15,10 +16,24 @@ import com.example.xiyou3g.thefriendsofthecity.R;
  */
 
 public class AboutFragment extends Fragment {
+    private View rootView;
+    private TextView signTextview;
+    private TextView locationTextview;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_person_about,container,false);
+
+        rootView= inflater.inflate(R.layout.fragment_person_about,container,false);
+        initView(rootView);
+        return rootView;
+    }
+
+    private void initView(View rootView) {
+        signTextview = (TextView)rootView.findViewById(R.id.sign_textview);
+        locationTextview = (TextView)rootView.findViewById(R.id.location_textview);
+
+        signTextview.setText(" 如果你无法简洁的表达你的想法，那只说明你还不够了解他\n         --阿尔伯特·爱因斯坦");
+        locationTextview.setText(" 陕西省西安市");
     }
 
     @Override
