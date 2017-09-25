91c67fcd4e241e24e740ea7d085d132ddd0980a1
wangjiapu <857484057@qq.com>
2017-09-26 00:39:56 +0800

    接口文档测试

--- app/src/main/java/activitys/LoginActivity.java
+++ app/src/main/java/activitys/LoginActivity.java
@@ -1,21 +1,35 @@
 package activitys;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.xiyou3g.thefriendsofthecity.R;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import utils.OkhttpUtil;
 
 /**
  * Created by xiyou3g on 2017/9/25.
  */
 
-public class LoginActivity  extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     LinearLayout signin;
     LinearLayout signup;
     Button signInButton;
@@ -23,13 +37,52 @@ public class LoginActivity  extends AppCompatActivity implements View.OnClickLis
     Button back;
     //界面好丑
 
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 200) {
+                Log.e("qqqqq", msg.obj.toString());
+            } else if (msg.what == 1) {
+                Log.e("qqqqq", msg.obj.toString());
+            } else {
+                Log.e("qqqqq", "pppppppppp");
+            }
+
+        }
+    };
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
         initView();
-       initBind();
+        initBind();
+
+        /**
+         * 暂时不要删这个
+         *
+         */
+
+        RequestBody formBody = new FormBody.Builder()
+                .build();
+        Request request = OkhttpUtil
+                .getRequest(OkhttpUtil.getHOST() + OkhttpUtil.getAllProvince(),formBody);
+        Call call = OkhttpUtil.getOkHttpClient().newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Message message = new Message();
+                message.what = 1;
+                message.obj = response.body().string();
+                handler.sendMessage(message);
+            }
+        });
 
     }
 
@@ -48,6 +101,7 @@ public class LoginActivity  extends AppCompatActivity implements View.OnClickLis
         signOnText.setOnClickListener(this);
         back.setOnClickListener(this);
     }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
--- app/src/main/java/utils/OkhttpUtil.java
+++ app/src/main/java/utils/OkhttpUtil.java
@@ -2,6 +2,7 @@ package utils;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class OkhttpUtil {
 
@@ -15,9 +16,66 @@ public class OkhttpUtil {
 
     private static final String DistrictsByCityId="/place/getDistrictsByCityId";
 
+    private static final String Sms="/user/sendSMS";
 
-    private static Request getRequest(String url){
-        return null;
+    private static final String Register="/user/register";
+
+    private static final String Login="/user/login";
+
+    private static final String Quit="/user/quit";
+
+    private static final String LoginUserInfo="/user/getLoginUserInfo";
+
+    public static Request getRequest(String url,RequestBody body){
+
+        Request request=new Request.Builder().url(url)
+                .addHeader("Content-Type","application/x-www-form-urlencoded")
+                .post(body)
+                .build();
+        return request;
     }
 
+    public static OkHttpClient getOkHttpClient() {
+        return okHttpClient;
+    }
+
+    public static void setOkHttpClient(OkHttpClient okHttpClient) {
+        OkhttpUtil.okHttpClient = okHttpClient;
+    }
+
+    public static String getHOST() {
+        return HOST;
+    }
+
+    public static String getAllProvince() {
+        return AllProvince;
+    }
+
+    public static String getCitiesByProvinceId() {
+        return CitiesByProvinceId;
+    }
+
+    public static String getDistrictsByCityId() {
+        return DistrictsByCityId;
+    }
+
+    public static String getSms() {
+        return Sms;
+    }
+
+    public static String getRegister() {
+        return Register;
+    }
+
+    public static String getLogin() {
+        return Login;
+    }
+
+    public static String getQuit() {
+        return Quit;
+    }
+
+    public static String getLoginUserInfo() {
+        return LoginUserInfo;
+    }
 }
