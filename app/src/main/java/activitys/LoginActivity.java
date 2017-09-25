ac010edbbfecf2b80687fca89e5ac91706334a4d
jiangtingting <18792898826@163.com>
2017-09-25 20:55:47 +0800

    登录注册界面

--- app/src/main/java/activitys/LoginActivity.java
+++ app/src/main/java/activitys/LoginActivity.java
@@ -1,9 +1,13 @@
 package activitys;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.xiyou3g.thefriendsofthecity.R;
 
@@ -11,10 +15,46 @@ import com.example.xiyou3g.thefriendsofthecity.R;
  * Created by xiyou3g on 2017/9/25.
  */
 
-public class LoginActivity  extends AppCompatActivity{
+public class LoginActivity  extends AppCompatActivity implements View.OnClickListener {
+    LinearLayout signin;
+    LinearLayout signup;
+    Button signInButton;
+    TextView signOnText;
+    Button back;
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+
+        signInButton = (Button) findViewById(R.id.sign_in_button);
+        signOnText = (TextView) findViewById(R.id.sign_up_text);
+        signin = (LinearLayout) findViewById(R.id.sign_in_layout);
+        signup = (LinearLayout) findViewById(R.id.sign_up_layout);
+        back = (Button) findViewById(R.id.chuce_back_button);
+        signin.setVisibility(View.VISIBLE);
+        signup.setVisibility(View.INVISIBLE);
+        signInButton.setOnClickListener(this);
+        signOnText.setOnClickListener(this);
+        back.setOnClickListener(this);
     }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sign_up_text:
+                signin.setVisibility(View.INVISIBLE);
+                signup.setVisibility(View.VISIBLE);
+                break;
+            case R.id.chuce_back_button:
+                signup.setVisibility(View.INVISIBLE);
+                signin.setVisibility(View.VISIBLE);
+                break;
+        }
+
+    }
+
+
 }
