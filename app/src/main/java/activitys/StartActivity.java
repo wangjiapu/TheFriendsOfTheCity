f3b1b80229c0041d4ecc1dde3fae53b8f5842eaf
jiangtingting <18792898826@163.com>
2017-09-29 21:38:43 +0800

    加了个权限

--- app/src/main/java/activitys/StartActivity.java
+++ app/src/main/java/activitys/StartActivity.java
@@ -1,10 +1,15 @@
 package activitys;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.WindowManager;
@@ -20,6 +25,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 import beans.UserInfo;
@@ -99,7 +106,6 @@ public class StartActivity extends AppCompatActivity {
                    isGo();
                }
            }else if (msg.what==4){
-               initLocation();
                if (msg.obj.equals("获取同城书友书失败!")){
                    Log.e("444444","error");
                    isGo();
@@ -120,14 +126,19 @@ public class StartActivity extends AppCompatActivity {
         localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS |
                 localLayoutParams.flags);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+
         mListener = new MyLocationListener();
-        initLocation();
-        if (!mLocationClient.isStarted()) {
-            mLocationClient.start();
-        }
+initLocation();
+        setContentView(R.layout.activity_start);
+
+
+        permission();
+
 //        initData();
     }
+
+
+
 
     public class MyLocationListener extends BDAbstractLocationListener {
         @Override
@@ -232,6 +243,9 @@ public class StartActivity extends AppCompatActivity {
 
     private void initLocation() {
         mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(mListener);
+
+
         LocationClientOption option = new LocationClientOption();
         option.setIsNeedAddress(true);
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
@@ -239,13 +253,72 @@ public class StartActivity extends AppCompatActivity {
 //        int span=1000;
 //        option.setScanSpan(span);
         mLocationClient.setLocOption(option);
-        mLocationClient.registerLocationListener(mListener);
+
     }
 
 
 
 
 
+    private void permission() {
+        List<String> permissionList = new ArrayList<>();
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (!permissionList.isEmpty()) {
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(this, permissions, 1);
+        } else {
+            requestLocation();
+        }
+    }
+
+//    private void requestLocation() {
+//        initLocation();
+//    }
+
+    private void requestLocation() {
+
+        if (!mLocationClient.isStarted()) {
+            mLocationClient.start();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0) {
+                    for (int result:grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(this, "需要权限", Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    requestLocation();
+                } else {
+                    Toast.makeText(this, "未知错误", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
 }
